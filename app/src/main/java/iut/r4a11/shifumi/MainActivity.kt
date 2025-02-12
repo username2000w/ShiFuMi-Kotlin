c5471625eebf9586818901e431adb6b8d8ea8029
Maxime <maximechagnon00@gmail.com>
2025-02-12 09:41:21 +0100

    navigation, button jouer

--- app/src/main/java/iut/r4a11/shifumi/MainActivity.kt
+++ app/src/main/java/iut/r4a11/shifumi/MainActivity.kt
@@ -8,30 +8,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import iut.r4a11.shifumi.ui.theme.ShiFuMiTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,6 +43,9 @@ fun AppNavigation() {
         composable("home") {
             HomeScreen(navController = navController)
         }
+        composable("game") {
+            GameScreen(navController = navController)
+        }
     }
 }
 
@@ -68,5 +60,14 @@ fun HomeScreen(navController: NavController) {
     ) {
         Text(text = "Bienvenue dans ShiFuMi",
             style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { navController.navigate("game") }) {
+            Text(text = "Jouer")
+        }
     }
+}
+
+@Composable
+fun GameScreen(navController: NavController) {
+    Text(text = "Joue")
 }
